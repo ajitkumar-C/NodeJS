27712bead3bca35d813640d63d1fdfd39faab90a
ajitrsa <ajitkumar.chaurasiya@rsagroup.ca>
2023-04-01 11:54:43 +0530

    code commit for fs (file system ) functions

---
+++ Inbuilt/fssync.js
@@ -0,0 +1,8 @@
+let fs=require('fs');
+
+let data=fs.readFileSync('city.json',{encoding:'utf-8',flag:'r'})
+console.log(data)
+
+let data1=fs.readFileSync('myfile.txt',{encoding:'utf-8',flag:'r'})
+console.log(data1)
+
